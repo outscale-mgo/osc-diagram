3ff7323dee130c41a67cd7b9173b1506a84de7a2
Matthias Gatto <matthias.gatto@outscale.com>
2023-03-20 19:08:20 +0100

    with volumes
    Signed-off-by: Matthias Gatto <matthias.gatto@outscale.com>

--- osc_diagram/main.py
+++ osc_diagram/main.py
@@ -3,6 +3,7 @@ from libcloud.compute.providers import get_driver
 from diagrams import Cluster, Diagram
 from diagrams.outscale.compute import Compute
 from diagrams.outscale.security import IdentityAndAccessManagement
+from diagrams.outscale.storage import Storage 
 import os
 
 key = os.environ['OSC_ACCESS_KEY']
@@ -18,16 +19,22 @@ def main():
 
     print(nodes)
 
-    with Diagram("All Vms", show=False):
+    with Diagram("All Vms", outformat=["png", "dot"], direction="BT"):
         for n in nodes:
             print(n.name, ": ", n.extra['VmId'])
             vm = Compute(n.name)
-            with Cluster("SecurityGroups"):
+            with Cluster("SG:\n" + n.name + '\n' + n.extra['VmId']):
                 sgs_cluster = []
                 for sg in  n.extra['SecurityGroups']:
                     sgs_cluster.append(IdentityAndAccessManagement(sg["SecurityGroupName"]))
                     print(sg)
+            with Cluster("Devs:\n" + n.name + '\n' + n.extra['VmId']):
+                bd_cluster = []
+                for bd in  n.extra['BlockDeviceMappings']:
+                    bd_cluster.append(Storage(bd["DeviceName"] + "\n" + bd["Bsu"]["VolumeId"]))
+                    print(sg)
             vm >> sgs_cluster
+            vm >> bd_cluster
 
 
 if __name__ == "__main__":
