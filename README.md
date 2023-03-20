761dea6f290abd902a8b44fa303e1910de4a5360
Matthias Gatto <matthias.gatto@outscale.com>
2023-03-20 15:04:54 +0100

    initial commit
    Signed-off-by: Matthias Gatto <matthias.gatto@outscale.com>

---
+++ README.md
@@ -0,0 +1,13 @@
+# Outscale Diagram
+
+This project serve as examples for libcloud/diagram, and as a lib to show diagrams of your cloud.
+
+### Installing from sources
+
+It is a good practice to create a [dedicated virtualenv](https://virtualenv.pypa.io/en/latest/) first. Even if it usually won't harm to install Python libraries directly on the system, better to contain \
+dependencies in a virtual environment.
+
+```bash
+python3 -m venv .venv
+source .venv/bin/activate
+```
---
+++ setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+import setuptools
+import os
+
+def get_long_description():
+    root_path = os.path.dirname(os.path.abspath(__file__))
+    with open(os.path.join(root_path, 'README.md'), 'r') as fd:
+        return fd.read()
+
+def get_version():
+    root_path = os.path.dirname(os.path.abspath(__file__))
+    with open(os.path.join(root_path, 'osc-diagram', 'VERSION'), 'r') as fd:
+        return fd.read().strip()
+
+setuptools.setup(
+    name='osc-diagram',
+    version=get_version(),
+    author="Outscal SAS",
+    author_email="opensource@outscale.com",
+    description="Outscale Gateway python SDK",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    url="https://github.com/outscale/osc-diagram",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'pyperclip>=3.7.0',
+        'diagrams>=0.23.3'
+    ]
+)
