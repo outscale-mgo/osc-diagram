3ae0bdd72a2c88072f77e343c948b15a9bbd4b51
matthias.gatto <matthias.gatto@outscale.con>
2023-05-04 18:12:46 +0200

    make osc-diagram usagble as a lib
    Signed-off-by: matthias.gatto <matthias.gatto@outscale.con>

--- osc_diagram/main.py
+++ osc_diagram/main.py
@@ -6,33 +6,36 @@ from diagrams.outscale.security import IdentityAndAccessManagement
 from diagrams.outscale.storage import Storage 
 import os
 
-key = os.environ['OSC_ACCESS_KEY']
-secret = os.environ["OSC_SECRET_KEY"]
+key = ""
+secret = ""
+
+try:
+    key = os.environ['OSC_ACCESS_KEY']
+    secret = os.environ["OSC_SECRET_KEY"]
+except:
+    pass
+
 region = "eu-west-2"
 service = "api"
 
-def main():
+def main(ak=key, sk=secret, format=["png", "dot"], region=region, service=service):
     Driver = get_driver(Provider.OUTSCALE)
-    driver = Driver(key=key, secret=secret, region=region, service=service)
+    driver = Driver(key=ak, secret=sk, region=region, service=service)
 
     nodes = driver.list_nodes()
 
-    print(nodes)
-
-    with Diagram("All Vms", outformat=["png", "dot"], direction="BT"):
+    with Diagram("All Vms", outformat=format, direction="BT"):
         for n in nodes:
-            print(n.name, ": ", n.extra['VmId'])
             vm = Compute(n.name)
             with Cluster("SG:\n" + n.name + '\n' + n.extra['VmId']):
                 sgs_cluster = []
                 for sg in  n.extra['SecurityGroups']:
                     sgs_cluster.append(IdentityAndAccessManagement(sg["SecurityGroupName"]))
-                    print(sg)
+                    
             with Cluster("Devs:\n" + n.name + '\n' + n.extra['VmId']):
                 bd_cluster = []
                 for bd in  n.extra['BlockDeviceMappings']:
                     bd_cluster.append(Storage(bd["DeviceName"] + "\n" + bd["Bsu"]["VolumeId"]))
-                    print(sg)
             vm >> sgs_cluster
             vm >> bd_cluster
 
