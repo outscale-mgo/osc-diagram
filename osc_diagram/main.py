f280328b963e77ce8fbf894123d7da7d61a1814c
Matthias Gatto <matthias.gatto@outscale.com>
2023-05-11 13:50:58 +0200

    better modif for once upstream
    Signed-off-by: Matthias Gatto <matthias.gatto@outscale.com>

--- osc_diagram/main.py
+++ osc_diagram/main.py
@@ -1,7 +1,7 @@
 from libcloud.compute.types import Provider
 from libcloud.compute.providers import get_driver
 from diagrams import Cluster, Diagram
-from diagrams.outscale.compute import Compute
+from diagrams.generic.virtualization import Qemu
 from diagrams.outscale.security import IdentityAndAccessManagement
 from diagrams.outscale.storage import Storage 
 import os
@@ -10,6 +10,12 @@ key = os.environ['OSC_ACCESS_KEY']
 secret = os.environ["OSC_SECRET_KEY"]
 region = "eu-west-2"
 service = "api"
+
+
+def sn(str):
+    if len(str) > 16:
+        return str[:14] + "..."
+    return str
 
 def main():
     Driver = get_driver(Provider.OUTSCALE)
@@ -21,20 +27,23 @@ def main():
 
     with Diagram("All Vms", outformat=["png", "dot"], direction="BT"):
         for n in nodes:
+            nextra = n.extra
             print(n.name, ": ", n.extra['VmId'])
-            vm = Compute(n.name)
-            with Cluster("SG:\n" + n.name + '\n' + n.extra['VmId']):
+            vm = Qemu(n.name + '\n`' + nextra['PublicIp'])
+            with Cluster("SG:\n" + (n.name if n.name else "(no name)") + '\n' + n.extra['VmId']):
                 sgs_cluster = []
                 for sg in  n.extra['SecurityGroups']:
-                    sgs_cluster.append(IdentityAndAccessManagement(sg["SecurityGroupName"]))
+                    sgs_cluster.append(IdentityAndAccessManagement(sn(sg["SecurityGroupName"])))
                     print(sg)
             with Cluster("Devs:\n" + n.name + '\n' + n.extra['VmId']):
                 bd_cluster = []
                 for bd in  n.extra['BlockDeviceMappings']:
-                    bd_cluster.append(Storage(bd["DeviceName"] + "\n" + bd["Bsu"]["VolumeId"]))
+                    vol = driver.list_volumes('{"Filters": { "VolumeIds":["' + bd["Bsu"]["VolumeId"] +  '"]}}')
+                    print(vol)
+                    bd_cluster.append(Storage(bd["DeviceName"] + "\n" + bd["Bsu"]["VolumeId"] + "\n" + str(vol[0].size) + "GB"))
                     print(sg)
             vm >> sgs_cluster
-            vm >> bd_cluster
+            bd_cluster << vm
 
 
 if __name__ == "__main__":
